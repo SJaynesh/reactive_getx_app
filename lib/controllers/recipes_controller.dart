7503a2af98f87542f998473e770d4b317a70ea8e
rwn3developer <rwn3developer11@gmail.com>
2024-12-12 12:01:14 +0530

    12/12/2024

--- lib/controllers/recipes_controller.dart
+++ lib/controllers/recipes_controller.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:reactive_getx_counter_app/models/recipes_model.dart';
 
 class RecipesController extends GetxController {
-  RxList<RecipesModel>? recipesList;
+  RxList<RecipesModel> recipesList = <RecipesModel>[].obs;
+  RxList<RecipesModel> addToCartList = <RecipesModel>[].obs;
 
   RecipesController() {
     loadRecipesJsonData();
@@ -18,11 +20,18 @@ class RecipesController extends GetxController {
 
     List allRecipesData = jsonDecode(recipesData);
 
-    recipesList?.value = allRecipesData
-            .map(
-              (e) => RecipesModel.fromJson(json: e),
-            )
-            .toList() ??
-        [];
+    recipesList.value = allRecipesData
+        .map(
+          (e) => RecipesModel.fromJson(json: e),
+        )
+        .toList();
+  }
+
+  void addToCartRecipes({required RecipesModel item}) {
+    addToCartList.add(item);
+
+    log("Cart List Length : ${addToCartList.length}");
+
+    Get.back();
   }
 }
--- lib/view/screen/e_commerce_page.dart
+++ lib/view/screen/e_commerce_page.dart
@@ -16,18 +16,100 @@ class ECommercePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Recipes"),
         centerTitle: true,
+        actions: [
+          Stack(
+            alignment: Alignment.topRight,
+            children: [
+              Icon(
+                Icons.shopping_cart,
+                size: 35,
+              ),
+              CircleAvatar(
+                radius: 8,
+                backgroundColor: Colors.red,
+                child: Transform.translate(
+                    offset: const Offset(0, -2),
+                    child: Obx(
+                      () => Text(
+                        "${controller.addToCartList.length}",
+                        style: const TextStyle(
+                          fontSize: 14,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )),
+              ),
+            ],
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+        ],
         backgroundColor: Colors.grey.withOpacity(0.4),
       ),
       backgroundColor: Colors.white60,
-      body: (controller.recipesList != null)
+      body: (controller.recipesList.isNotEmpty)
           ? GridView.builder(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
+                childAspectRatio: 0.8,
               ),
-              itemBuilder: (context, index) => Card(
-                child: Container(),
-              ),
-              itemCount: controller.recipesList?.length,
+              itemBuilder: (context, index) {
+                var item = controller.recipesList[index];
+                return GestureDetector(
+                  onTap: () {
+                    Get.defaultDialog(
+                      title: "ADD TO CART",
+                      content: Column(
+                        children: [
+                          Text(item.name),
+                          ElevatedButton(
+                            onPressed: () {
+                              controller.addToCartRecipes(item: item);
+                            },
+                            child: const Text("Add to cart"),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 150,
+                            margin: const EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              image: DecorationImage(
+                                image: NetworkImage(item.image),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            item.name,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "⭐ ${item.rating}",
+                            style: const TextStyle(
+                                fontSize: 16, fontStyle: FontStyle.italic),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+              itemCount: controller.recipesList.length,
             )
           : const Center(
               child: Text("No Data"),
